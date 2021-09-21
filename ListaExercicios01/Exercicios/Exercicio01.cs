ffb26d978a3d6a9e88f88523d06979812cf6606d
Rodrigo Tomagnini <rodrigort@gmail.com>
2021-09-21 19:12:34 +0200

    commit

---
+++ ListaExercicios01/Exercicios/Exercicio01.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace ListaExercicios01
+{
+    class Exercicio01
+    {
+        public static void executar()
+        {
+            /*
+                Escreva um algoritmo que armazene o valor 10 em uma variável A e o
+                valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre
+                variáveis) troque os seus conteúdos fazendo com que o valor que está em
+                A passe para B e vice-versa. Ao final, escrever os valores que ficaram
+                armazenados nas variáveis.
+            */
+
+            int A = 10;
+            int B = 20;
+
+            B = A;
+            A = B;
+
+            Console.WriteLine("A:" + A + " - B: " + B);
+
+        }
+    }
+}
---
+++ ListaExercicios01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ListaExercicios01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** LISTA DE EXERCÍCIOS 01 ***");
+            int opcao = 0;
+
+            //Do While para que o programa execute até que seja digitado 99
+            do
+            {
+                //Mensagem para o usuario digitar o número do exercício
+                Console.Write(" Digite o número do exercício (99 - para sair): ");
+                //Captura e conversão a inteiro da opção digitada
+                int.TryParse(Console.ReadLine(), out opcao);
+
+                switch(opcao)
+                {
+                    case 1:
+                        Exercicio01.executar();
+                        break;
+                    case 2:
+                        //To-Do Exercicio 2
+                        break;
+                    case 99:
+                        Console.WriteLine(" * Saindo do programa");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+
+            }while(opcao != 99);
+        }
+    }
+}
