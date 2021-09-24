a9bd6c25d334dc93e156709674b4af9ee87f1c57
Rodrigo Tomagnini <rodrigort@gmail.com>
2021-09-24 15:48:18 +0200

    cmooit

--- Aula05/ExemploHeranca/Program.cs
+++ Aula05/ExemploHeranca/Program.cs
@@ -12,6 +12,38 @@ namespace ExemploHeranca
 
             Morcego vampiro = new Morcego("Batman");
 
+            /**************************************************
+                EXEMPLOS PRINCIPIOS DE SUBSTITUICAO
+            *************************************************/
+            Cavalo pangare = new Cavalo("Pangaré");
+            Mamiferos animal = new Mamiferos("Mamifero");
+
+            animal = pangare;
+            animal.getNomeAnimal();
+            animal = sapiens;
+            animal.getNomeAnimal();
+            animal = vampiro;
+            animal.getNomeAnimal();
+
+            Mamiferos mamifero1 = new Cavalo("Pangaré");
+
+            //Exemplo armazenar em um Array, diferentes tipos de mamífero
+            Mamiferos[] arrayAnimais = new Mamiferos[4];
+            arrayAnimais[0] = mangalarga;
+            arrayAnimais[1] = pangare;
+            arrayAnimais[2] = sapiens;
+            arrayAnimais[3] = vampiro;
+
+            //Classe Object nesse caso seria a classe Avô, ela pode
+            //receber as subclasses, mas só terá acesso aos seus 
+            //métodos proprios/excluivos
+            Object obj = pangare;
+            
+
+            Console.WriteLine("--------------------------");
+
+            
+
             Console.WriteLine("******* CAVALO ********");
             mangalarga.getNomeAnimal();
             mangalarga.CuidarFilhotes();
@@ -26,8 +58,6 @@ namespace ExemploHeranca
             Console.WriteLine("******* MORCEGO ********");
             vampiro.getNomeAnimal();
             vampiro.Voar();
-
-            
         }
     }
 }
